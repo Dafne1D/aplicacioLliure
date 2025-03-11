24a883851cccb30306b89c78c15fda424b9db1ed
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-11 12:51:37 +0100

    Creació getNomJugador, getCartes, posible Constructor

--- Jugador.java
+++ Jugador.java
@@ -1,6 +1,22 @@
 package UNO;
 
+import java.util.ArrayList;
+
 public class Jugador {
     public String NomJugador;
 
+    private ArrayList<Carta> cartes = new ArrayList<Carta>();
+
+    //Constructor
+    private Jugador (int NomJugador){
+        NomJugador = NomJugador;
+    }
+
+    public String getNomJugador(){
+        return NomJugador;
+    }
+
+    public ArrayList<Carta> getCartes(){
+        return cartes;
+    }
 }
