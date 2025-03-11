a2dc823f8239966ec1849f870e3a4e43e0208de3
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-11 12:52:06 +0100

    Posible constructor carta.java

--- Carta.java
+++ Carta.java
@@ -3,11 +3,21 @@ package UNO;
 public class Carta {
     public int NumCarta;
 
-    public enum colors{
+    public enum Colors{
         vermell,
         groc,
         blau,
         verd
     }
+    private int IdCarta;
+    private Colors color;
 
+
+
+
+    //Constructor
+    private Carta (int IdCarta, Colors color){
+        IdCarta = IdCarta;
+        color = color;
+    }
 }
