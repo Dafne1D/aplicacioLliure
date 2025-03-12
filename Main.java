7de5f51bc66da8e9ca34e95f80803d329b97d2fb
DafneD1 <dcarrillo572@boscdelacoma.cat>
2025-03-12 22:15:57 +0100

    mostrar les cartes

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 package UNO;
-
-
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        List<Carta> _crearBaralla= Carta.crearBaralla(40);
+        Carta.showBaraja(_crearBaralla);
+    }
 }
