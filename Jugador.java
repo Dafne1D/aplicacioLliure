2ddd08a5962fd23ff879900216c36e60c2417241
DafneD1 <dcarrillo572@boscdelacoma.cat>
2025-03-13 21:17:49 +0100

    ArrayList<Carta>

--- Jugador.java
+++ Jugador.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 public class Jugador {
     public String NomJugador;
 
-    private ArrayList<Carta> cartes = new ArrayList<Carta>();
+    private ArrayList<Carta> baralla = new ArrayList<Carta>();
 
     //Constructor
     private Jugador (int NomJugador){
         NomJugador = NomJugador;
     }
 
+
+
+    //GETS---------------------------------------------------------
     public String getNomJugador(){
         return NomJugador;
     }
 
     public ArrayList<Carta> getCartes(){
-        return cartes;
+        return baralla;
     }
 }
