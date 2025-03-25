7cd08d2d24e853875304d0571d2098620c08b150
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-25 10:40:25 +0100

    Update: Main d'en Joan

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 package UNO;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        List<Carta> _crearBaralla= Carta.crearBaralla();
-        Carta.showBaraja(_crearBaralla);
+
+        Carta c1 = new Carta(1, Carta.Colors.groc);
+        Carta c2 = new Carta(3, Carta.Colors.vermell);
+        Carta c3 = new Carta(6, Carta.Colors.blau);
+        Carta c4 = new Carta(9, Carta.Colors.verd);
+
+        Jugador j1 = new Jugador("Jugador 1");
+        j1.addCarta(c1);
+        j1.addCarta(c2);
+        j1.addCarta(c3);
+        j1.addCarta(c4);
+
+        for (Carta c : j1.getCarta()) {
+            UI.mostrarCarta(c);
+        }
     }
 }
