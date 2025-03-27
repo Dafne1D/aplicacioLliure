98fddb65d2086685a1ae712d6af419d7958866c0
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-27 09:25:12 +0100

    Mostrar cartes

--- Main.java
+++ Main.java
@@ -2,20 +2,17 @@ package UNO;
 
 public class Main {
     public static void main(String[] args) {
-
-        Carta c1 = new Carta(1, Carta.Colors.groc);
-        Carta c2 = new Carta(3, Carta.Colors.vermell);
-        Carta c3 = new Carta(6, Carta.Colors.blau);
-        Carta c4 = new Carta(9, Carta.Colors.verd);
-
         Jugador j1 = new Jugador("Jugador 1");
-        j1.addCarta(c1);
-        j1.addCarta(c2);
-        j1.addCarta(c3);
-        j1.addCarta(c4);
 
-        for (Carta c : j1.getCarta()) {
-            UI.mostrarCarta(c);
+        Mazo mazo1 = new Mazo();
+        System.out.println(mazo1);
+
+        for (Carta c : mazo1.getCartes()){
+            j1.addCarta(c);
         }
+
+        UI.mostrarCartes(j1.getCartes());
+
     }
+
 }
