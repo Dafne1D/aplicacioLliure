964bd37cd82105cd7ff2afceee9d53164bf57ace
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-27 10:17:06 +0100

    Update: Main

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 package UNO;
 
+
 public class Main {
     public static void main(String[] args) {
+
         Jugador j1 = new Jugador("Jugador 1");
 
         Mazo mazo1 = new Mazo();
-        System.out.println(mazo1);
-
         for (Carta c : mazo1.getCartes()){
             j1.addCarta(c);
         }
