eb40b09c2b27f47bf76549bf63c099d832ffb1b8
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-27 09:03:33 +0100

    Create: Constructor cartes

--- Mazo.java
+++ Mazo.java
@@ -1,4 +1,20 @@
 package UNO;
+import java.awt.*;
+import java.util.Stack;
 
 public class Mazo {
+    //Crear les cartes---------------------------------------------
+    Stack<Carta> cartes = new Stack<>();
+
+    public Mazo(){
+        for (Carta.Colors colors : Carta.Colors.values()){
+            cartes.push(new Carta(0, colors));
+            for (int i = 1; i <=9; i++){
+                cartes.push(new Carta(i, colors));
+            }
+
+        }
+
+    }
+
 }
