f9e92d0607ed9622fcfa4ec048eec5d4fa64f5db
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-27 10:51:06 +0100

    Update: creidar barrejar()

--- Main.java
+++ Main.java
@@ -4,13 +4,18 @@ package UNO;
 public class Main {
     public static void main(String[] args) {
 
+
         Jugador j1 = new Jugador("Jugador 1");
 
         Mazo mazo1 = new Mazo();
+        mazo1.barrejar();
+        Pilo pilo1 = new Pilo();
+        pilo1.getCartes();
+        System.out.println(pilo1);
+
         for (Carta c : mazo1.getCartes()){
             j1.addCarta(c);
         }
-
         UI.mostrarCartes(j1.getCartes());
 
     }
