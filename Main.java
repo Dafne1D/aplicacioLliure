f7b69cb5a8aef029d10bb97509b329dd61d1879d
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-27 11:06:39 +0100

    Create: mostrar el pilo

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         mazo1.barrejar();
         Pilo pilo1 = new Pilo();
         pilo1.getCartes();
-        for (int i = 1; i<7; i++){
+        for (int i = 0; i<38; i++){
             pilo1.getCartes().push(mazo1.agafarCarta());
         }
         System.out.println("PILO");
