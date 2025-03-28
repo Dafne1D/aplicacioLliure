eb28ea64e9be286c534d8aefb869cc58bea7e40f
Dafne <dcarrillo572@boscdelacoma.cat>
2025-03-28 09:41:21 +0100

    Update: importar carpetes - logic, interficie, uno

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
         mazo1.barrejar();
         Pilo pilo1 = new Pilo();
         pilo1.getCartes();
-        for (int i = 0; i<38; i++){
+        for (int i = 0; i<1; i++){
             pilo1.getCartes().push(mazo1.agafarCarta());
         }
         System.out.println("PILO");
---
+++ logica/Jugador.java
@@ -0,0 +1,32 @@
+package uno.logica;
+
+import java.util.ArrayList;
+
+public class Jugador {
+
+
+    private String nom;
+    public ArrayList<Carta> cartes; //
+
+    public Jugador(String nomJugador){
+        nomJugador = nom;
+        cartes = new ArrayList<>();
+    }
+
+    public String getNom(String nom) {
+        return nom;
+    }
+    public ArrayList<Carta> getCartes() {
+        return cartes;
+    }
+
+    public void addCarta(Carta carta){
+        cartes.add(carta);
+    }
+
+    public void tirarCarta(Pilo pilo, Carta carta){
+        pilo.getCartes().push(carta);
+        cartes.remove(carta);
+    }
+
+}
